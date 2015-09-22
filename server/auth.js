e40d946b8c57aa9c00e70db2f291cf456722cd4d
Ashutosh Das <git.pyprism@gmail.com>
2015-09-22 11:02:57 +0600

    remove meteor. welcome django :smile:

--- server/auth.js
+++
@@ -1,6 +0,0 @@
-/**
- * Created by prism on 8/16/15.
- */
-
-var database = new MongoInternals.RemoteCollectionDriver("mongodb://127.0.0.1:3001/hiren_pirate");
-User = new Mongo.Collection("user", { _driver: database });
