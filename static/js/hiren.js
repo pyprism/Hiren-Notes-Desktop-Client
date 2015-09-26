4f982c8f971456a30b62612377518a73f6b50df7
Ashutosh Das <git.pyprism@gmail.com>
2015-09-26 20:18:39 +0600

    remove raw django

--- static/js/hiren.js
+++
@@ -1,9 +0,0 @@
-tinymce.init({
-    selector: "textarea",
-    plugins: [
-        "advlist autolink lists link charmap preview anchor",
-        "searchreplace visualblocks code fullscreen",
-        "insertdatetime media table contextmenu paste"
-    ],
-    toolbar: "undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image"
-});
