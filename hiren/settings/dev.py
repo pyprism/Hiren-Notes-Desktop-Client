64051b0c002e1524c9ad76151afecf9be68a1f95
Ashutosh Das <git.pyprism@gmail.com>
2015-09-27 11:17:47 +0600

    fuck cms

--- hiren/settings/dev.py
+++ hiren/settings/dev.py
@@ -7,10 +7,10 @@ DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
 try:
-    with open('config.json') as f:
+    with open('config.local.json') as f:
         JSON_DATA = json.load(f)
 except FileNotFoundError:
-    with open('config.local.json') as f:
+    with open('config.json') as f:
         JSON_DATA = json.load(f)
 
 SECRET_KEY = os.environ.get('SECRET_KEY', JSON_DATA['secret_key'])
