6c618ab9569b2967f02e88384c2ce3a32257b57b
Ashutosh Das <git.pyprism@gmail.com>
2015-10-15 00:56:00 +0600

    add user model

--- api/models/User.js
+++ api/models/User.js
@@ -4,11 +4,39 @@
 * @description :: TODO: You might write a short summary of how this model works and what it represents here.
 * @docs        :: http://sailsjs.org/#!documentation/models
 */
+var bcrypt = require('bcrypt');
 
 module.exports = {
 
   attributes: {
+    email: {
+      type: 'email',
+      required: true
+    },
+    password: {
+      type: 'string',
+      minLength: 6,
+      required: true
+    },
+    toJSON: function() {
+      var obj = this.toObject();
+      delete obj.password;
+      return obj;
+    }
+  },
 
+  beforeCreate: function(user, cb) {
+    bcrypt.genSalt(10, function(err, salt) {
+      bcrypt.hash(user.password, salt, function(err, hash) {
+        if (err) {
+          console.log(err);
+          cb(err);
+        } else {
+          user.password = hash;
+          cb();
+        }
+      });
+    });
   }
 };
 
