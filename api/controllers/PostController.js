3db1c92270178def073c8d8c6c76de287f7fd844
Ashutosh Das <git.pyprism@gmail.com>
2015-10-15 20:23:16 +0600

    update post controller

--- api/controllers/PostController.js
+++ api/controllers/PostController.js
@@ -6,6 +6,20 @@
  */
 
 module.exports = {
-	
+
+  index: function(req, res) {
+    res.render('admin/dashboard', {'saved': false});
+  },
+
+  editor: function(req, res) {
+    Post.create({title:req.body.title, content:req.body.content, category:req.body.category}).exec(function createPost(err, data) {
+      if (err) {
+        return res.json(err);
+      }
+      else {
+        return res.view('admin/dashboard', {'saved':true, 'data': data.id});
+      }
+    });
+  }
 };
 
