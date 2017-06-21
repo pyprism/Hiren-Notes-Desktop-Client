7a221e07724ed1e7c71dba642660a98fba152772
Ashutosh Das <git.pyprism@gmail.com>
2017-06-21 21:54:05 +0600

    added app info

--- desktop/notes/main.cpp
+++ desktop/notes/main.cpp
@@ -3,9 +3,14 @@
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
+    QApplication app(argc, argv);
+    app.setOrganizationName ("pyprism");
+    app.setOrganizationDomain("pyprism.me");
+    app.setApplicationName ("Hiren_Note");
+    app.setApplicationDisplayName ("Hiren Note");
+    app.setWindowIcon(QIcon(":/new/img/text_editor.svg"));
     LoginWindow w;
     w.show();
 
-    return a.exec();
+    return app.exec();
 }
