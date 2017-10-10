4d739896e5e50f984a6865e77f95646904dd0ded
Ashutosh Das <git.pyprism@gmail.com>
2017-10-10 18:51:06 +0600

    changed name

--- desktop/notes/main.cpp
+++ desktop/notes/main.cpp
@@ -6,8 +6,8 @@ int main(int argc, char *argv[])
     QApplication app(argc, argv);
     app.setOrganizationName ("pyprism");
     app.setOrganizationDomain("pyprism.me");
-    app.setApplicationName ("Hiren_Note");
-    app.setApplicationDisplayName ("Hiren Note");
+    app.setApplicationName ("Hiren_Notes");
+    app.setApplicationDisplayName ("Hiren Notes");
     app.setWindowIcon(QIcon(":/new/img/text_editor.svg"));
     LoginWindow w;
     w.show();
